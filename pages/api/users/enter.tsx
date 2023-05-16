a0dfc04892cf80b6a4c65e3ef996f1b4bdc24756
Anchangwan <ssdii44@naver.com>
2023-05-16 17:27:52 +0900

    prisma upsert on handler

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,59 @@
 import type { NextApiRequest, NextApiResponse } from "next/types";
-import withHandler from "./../../../libs/server/withHandler";
+import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { PrismaClient } from "@prisma/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "익명",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+  // if (email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email,
+  //     },
+  //   });
+  //   if (user) console.log("찾음");
+  //   if (!user) {
+  //     console.log("찾지 못했음");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "익명",
+  //         email,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+  // if (phone) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   });
+  //   if (user) console.log("찾음");
+  //   if (!user) {
+  //     console.log("찾지 못했음");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "익명",
+  //         phone: +phone,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+
   res.status(200).end();
 }
 
