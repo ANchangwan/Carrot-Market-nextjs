cce086b1b25dd2df18f3751fa0dc5ec47df09213
Anchangwan <ssdii44@naver.com>
2023-05-17 15:56:27 +0900

    Token logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,27 +1,27 @@
 import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
 import type { NextApiRequest, NextApiResponse } from "next/types";
-<<<<<<< HEAD
-import withHandler from "@libs/server/withHandler";
-import client from "@libs/server/client";
 import { PrismaClient } from "@prisma/client";
-=======
->>>>>>> 1f4c68ac3c7b3c1e565646235c5c7016310940cf
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      payload: "1234",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "익명",
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: "익명",
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
   // if (email) {
   //   user = await client.user.findUnique({
   //     where: {
