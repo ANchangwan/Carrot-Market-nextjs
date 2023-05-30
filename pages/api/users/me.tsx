b35016c78b278fd91cf0840b6e8959d749208d0d
Anchangwan <ssdii44@naver.com>
2023-05-30 16:03:37 +0900

    apiSession and me 파일 생성

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -15,22 +15,18 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { token } = req.body;
-  const exists = await client.token.findUnique({
+  const profile = await client.user.findUnique({
     where: {
-      payload: token,
+      id: req.session.user?.id,
     },
-    include: { user: true },
   });
-  if (!exists) return res.status(404).end();
-  req.session.user = {
-    id: exists?.userId,
-  };
-  await req.session.save();
-  res.status(200).end();
+  res.json({
+    ok: true,
+    profile,
+  });
 }
 
-export default withIronSessionApiRoute(withHandler("POST", handler), {
+export default withIronSessionApiRoute(withHandler("GET", handler), {
   cookieName: "carrotsession",
   password:
     "7897894654564984798465465849844654849496dfsdfsdfsdfsdfasdfasdfaefwfwfsdfsdfwefw",
