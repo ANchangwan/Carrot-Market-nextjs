5b1e302c42ea63395132f43ae12bc959d2d3e7e3
Anchangwan <ssdii44@naver.com>
2023-05-30 17:59:18 +0900

    withSession 추가

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,37 +1,33 @@
-import { withIronSessionApiRoute } from "iron-session/next";
 import client from "@libs/server/client";
 import withHandler, { RespoonseType } from "@libs/server/withHandler";
 import type { NextApiRequest, NextApiResponse } from "next/types";
-
-declare module "iron-session" {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    };
-  }
-}
+import { withApiSession } from "@libs/server/withSession";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  const exists = await client.token.findUnique({
+  const foundToken = await client.token.findUnique({
     where: {
       payload: token,
     },
     include: { user: true },
   });
-  if (!exists) return res.status(404).end();
+  console.log(foundToken);
+  if (!foundToken) return res.status(404).end();
   req.session.user = {
-    id: exists?.userId,
+    id: foundToken?.userId,
   };
   await req.session.save();
-  res.status(200).end();
+  await client.token.deleteMany({
+    where: {
+      userId: foundToken.userId,
+    },
+  });
+  res.json({
+    ok: true,
+  });
 }
 
-export default withIronSessionApiRoute(withHandler("POST", handler), {
-  cookieName: "carrotsession",
-  password:
-    "7897894654564984798465465849844654849496dfsdfsdfsdfsdfasdfasdfaefwfwfsdfsdfwefw",
-});
+export default withApiSession(withHandler("POST", handler));
--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -2,14 +2,7 @@ import { withIronSessionApiRoute } from "iron-session/next";
 import client from "@libs/server/client";
 import withHandler, { RespoonseType } from "@libs/server/withHandler";
 import type { NextApiRequest, NextApiResponse } from "next/types";
-
-declare module "iron-session" {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    };
-  }
-}
+import { withApiSession } from "@libs/server/withSession";
 
 async function handler(
   req: NextApiRequest,
@@ -26,8 +19,4 @@ async function handler(
   });
 }
 
-export default withIronSessionApiRoute(withHandler("GET", handler), {
-  cookieName: "carrotsession",
-  password:
-    "7897894654564984798465465849844654849496dfsdfsdfsdfsdfasdfasdfaefwfwfsdfsdfwefw",
-});
+export default withApiSession(withHandler("GET", handler));
