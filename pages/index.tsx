615799f5b1e529d35a80151a0696360b8450ff19
Anchangwan <ssdii44@naver.com>
2023-06-07 16:20:27 +0900

    useUse 와 replace 사용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import FloatingButton from "@components/floating-button";
 import Item from "@components/item";
 import Layout from "@components/layout";
+import useUser from "@libs/client/useUser";
+import Head from "next/head";
 
 function Home() {
+  const user = useUser();
+  console.log(user);
   return (
     <Layout title="홈" hasTabBar>
+      <Head>
+        <title>Home</title>
+      </Head>
       <div className="flex flex-col space-y-5 divide-y">
         {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
           <Item
