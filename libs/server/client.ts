e76f14e84acc22e03906d37863de02c3522cdc96
Anchangwan <ssdii44@naver.com>
2023-06-15 15:06:55 +0900

    product model

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
+declare global {
+  var client: PrismaClient | undefined;
+}
+
+const client = global.cient || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") {
+  global.client = clientl;
+}
+
 export default new PrismaClient();
