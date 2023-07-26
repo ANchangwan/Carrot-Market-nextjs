7fd79cc2d370204d648caded6f4135fa6df4d445
Anchangwan <ssdii44@naver.com>
2023-07-26 17:53:13 +0900

    All Posts

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -78,8 +78,9 @@ function CommunityPostDetail() {
   useEffect(() => {
     if (answerData && answerData.ok) {
       reset();
+      mutate();
     }
-  }, [answerData, reset]);
+  }, [answerData, reset, mutate]);
   return (
     <Layout canGoBack>
       <div>
